f68e232901b2772946b3ea534f05315c0dd72995
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2022-11-14 09:49:42 -0500

    created the main method

--- MyStack.java
+++ MyStack.java
@@ -4,3 +4,12 @@ Description: Implements a Stack data structure in Java.
 Teacher: Mr. Gugoiu
 Date: 11/14/22
 */
+
+import java.io.*;
+
+public class MyStack{
+    //main method, calls the methods on the stack
+    public static void main(String[] args){
+        
+    }
+}
